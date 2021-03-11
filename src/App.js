d984f8af36db4c69762e4bc571ec325e4f26536d
LukeMcCann <developer-lukemccann@outlook.com>
2021-03-11 18:18:23 +0000

    Add basic post-create component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { PostCreate } from './components/post-create/post-create.component';
 
 export default () => {
     return (
-        <div>Blog app</div>
+        <div className="container">
+            <h1>Create Post</h1>
+            <PostCreate />
+        </div>
     );
 };
