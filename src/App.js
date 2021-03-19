b1e14b368083dcbf29e5fcdbc06ff30f13264d4b
LukeMcCann <developer-lukemccann@outlook.com>
2021-03-19 21:17:42 +0000

    Update client

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { PostCreate } from './components/post-create/post-create.component';
-import { PostList } from './components/post-list/post-list.component';
+import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 export default () => {
-    return (
-        <div className="container">
-            <h1>Create Post</h1>
-            <PostCreate />
-            <hr />
-            <h1>Posts</h1>
-            <PostList />
-        </div>
-    );
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
+    </div>
+  );
 };
